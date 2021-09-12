993e1728e577d8153a72ec95cc644a600ddede7f
ceedel <ciddydella@gmail.com>
2021-09-12 11:11:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=10">
+    <meta http-equiv="X-UA-Compatable" content="ie=edge">
+
+    <!---Stylesheets section-->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <title>Home | Standard Bank Blog</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+                <div class="logo">
+                    <a href="index.html"><img src="assets/img/logo.png" alt="SBBlog logo"></a>
+                </div>
+                <nav>
+                    <a href="index.html">Home</a>
+                    <a href="about.html">About</a>
+                    <a href="contact.html">Contact</a>
+                    <a href="register.html">Register</a>
+            </nav>
+            <hr>
+        </header>
+        <main>
+            <section>
+                <h2>Blog post 1</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quos! Quidem praesentium iure mollitia inventore totam nihil assumenda officiis accusamus animi nulla aut exercitationem earum delectus, incidunt repellendus explicabo vero!</p>
+            </section>
+            <hr>
+            <section>
+                <h2>Blog post 2</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quos! Quidem praesentium iure mollitia inventore totam nihil assumenda officiis accusamus animi nulla aut exercitationem earum delectus, incidunt repellendus explicabo vero!</p>
+            </section>
+            <hr>
+            <section>
+                <h2>Blog post 3</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quos! Quidem praesentium iure mollitia inventore totam nihil assumenda officiis accusamus animi nulla aut exercitationem earum delectus, incidunt repellendus explicabo vero!</p>
+            </section>
+            <hr>
+            <section>
+                <h2>Blog post 4</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quos! Quidem praesentium iure mollitia inventore totam nihil assumenda officiis accusamus animi nulla aut exercitationem earum delectus, incidunt repellendus explicabo vero!</p>
+            </section>
+            <hr>
+            <section>
+                <h2>Blog post 5</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quos! Quidem praesentium iure mollitia inventore totam nihil assumenda officiis accusamus animi nulla aut exercitationem earum delectus, incidunt repellendus explicabo vero!</p>
+            </section>
+            <hr>
+        </main>
+
+        <aside>
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempore velit voluptatem aliquam dolorum exercitationem, aliquid soluta laboriosam fugiat delectus illum quis! Iste architecto sint atque explicabo minima aliquid soluta?</p>
+            <hr>
+            <h3>Recent Posts</h3>
+            <ul>
+                <li><a href="#">Blog Post 5</a></li>
+                <li><a href="#">Blog Post 4</a></li>
+                <li><a href="#">Blog Post 3</a></li>
+                <li><a href="#">Blog Post 2</a></li>
+                <li><a href="#">Blog Post 1</a></li>
+            </ul>
+        </aside>
+        <footer>
+            <section>
+                <a href="#">Register</a>
+                <a href="#">Login</a>
+                <a href="#">Post</a>
+                <a href="#">Contact</a>
+            </section>
+            <p align="center">Copyright &copy; 2021 standard bank cybersecurity interns. Website designed by the team.</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Register | Standard Bank Blog</title>
+    <h1>Blog registration form</h1>
+</head>
+<body>
+    <form> 
+        <table>
+            <tr>
+                <td>
+                    Email Address:
+                </td>
+    
+                <td>
+                    <input type=”mail”  placeholder=”Email” name=””>
+                </td>
+            </tr>
+    <tr>
+    <td>
+                    Password:
+                </td>
+    
+                <td>
+                    <input type=”Password” placeholder=”Password” name=””>
+                </td>
+            </tr>
+        </table>
+        <input type="checkbox" name="Male" value="Male"> Male<br> <input type="checkbox" name="Female" value="Female"> Female<br>
+    </form>
+</body>
+</html>
